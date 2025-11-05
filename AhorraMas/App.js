8b8f35666e4950fe23a7b1a1c202a64e3fb0b5b5
Elchema2005 <124051357@upq.edu.mx>
2025-11-05 09:09:47 -0600

    prueba

--- AhorraMas/App.js
+++ AhorraMas/App.js
@@ -1,16 +1,35 @@
-import PrincipalScreen from "./Screens/PrincipalScreen.js";
-import LoginScreen from "./Screens/LogInScreen.js";
-import RegistroAhorraApp from "./Screens/Registro.js";
-import PerfilyConfiguraciones from "./Screens/PerfilyConfiguraciones.js";
+import React, { useState } from "react";
+
+import LoginScreen from "./Screens/LogInScreen";
+import PrincipalScreen from "./Screens/PrincipalScreen";
+import PresupuestoScreen from "./Screens/PresupuestoScreen";
+import RegistroScreen from "./Screens/RegistroScreen";
+import PerfilyConfiguraciones from "./Screens/PerfilyConfiguraciones";
 
 export default function App() {
+  const [pantalla, setPantalla] = useState("Login"); 
 
   return (
+    <>
+      {pantalla === "Login" && (
+        <LoginScreen cambiarPantalla={setPantalla} />
+      )}
 
-    //<LoginScreen/>
-    //<PrincipalScreen/>
-    //<RegistroAhorraApp/>
-    <PerfilyConfiguraciones />
+      {pantalla === "Principal" && (
+        <PrincipalScreen cambiarPantalla={setPantalla} />
+      )}
 
+      {pantalla === "Presupuesto" && (
+        <PresupuestoScreen cambiarPantalla={setPantalla} />
+      )}
+
+      {pantalla === "Registro" && (
+        <RegistroScreen cambiarPantalla={setPantalla} />
+      )}
+
+      {pantalla === "Perfil" && (
+        <PerfilyConfiguraciones cambiarPantalla={setPantalla} />
+      )}
+    </>
   );
 }
