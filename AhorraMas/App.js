397caa57db01bfded30a630c764dedeb5d62184e
Elchema2005 <124051357@upq.edu.mx>
2025-11-04 22:42:23 -0600

    Presupuesto

--- AhorraMas/App.js
+++ AhorraMas/App.js
@@ -1,12 +1,12 @@
+import React from "react";
+import PresupuestoScreen from "./Screens/PresupuestoScreen.js";
 import PrincipalScreen from "./Screens/PrincipalScreen.js";
 import LoginScreen from "./Screens/LogInScreen.js";
 
 export default function App() {
-
   return (
-
-    <LoginScreen/>
-    //<PrincipalScreen/>
-
+    <PresupuestoScreen />
+    //<PrincipalScreen />
+    //<LoginScreen />
   );
 }
